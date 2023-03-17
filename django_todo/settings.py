61935428dcd32f50ffec1fef46d4fe0a86dff1d2
rpf13 <rpf13@me.com>
2023-03-17 09:18:46 +0000

    Add heroku url to list of allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://django-todo-rpf13.herokuapp.com']
 
 
 # Application definition
